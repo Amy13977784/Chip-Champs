935ade005fed8d02b5c0220cc0d9deb0005842ce
Amy <amy.rebk@gmail.com>
2025-01-09 12:56:48 +0100

    Connections begin af

--- representatie.py
+++ representatie.py
@@ -5,8 +5,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_grid(gates_path):
-    gates = pd.read_csv(gates_path)
+def plot_grid(connections_path, gates_path):
+    gates = pd.read_csv(gates_path, index_col = 'chip')
 
     x_max = max(gates['x']) + 1
     y_max = max(gates['y']) + 1
@@ -17,10 +17,13 @@ def plot_grid(gates_path):
     for line in range(1, y_max):
         plt.axhline(line, color = 'black', linewidth = 0.75)
 
+
+    plot_connections(connections_path, gates)
+
     plt.plot(gates['x'], gates['y'], 'rs', markersize = 29 - max([x_max, y_max]))
 
-    for _, row in gates.iterrows():
-        plt.text(row['x'], row['y'], row['chip'], fontsize = 27 - max([x_max, y_max]), \
+    for index, row in gates.iterrows():
+        plt.text(row['x'], row['y'], index, fontsize = 27 - max([x_max, y_max]), \
             horizontalalignment='center', verticalalignment='center_baseline')
 
     plt.xticks([])
@@ -31,12 +34,24 @@ def plot_grid(gates_path):
     plt.show()
 
 
-plot_grid('gates&netlists/chip_1/print_1.csv')
+def plot_connections(connections_path, gates):
+    connections = pd.read_csv(connections_path)
+   
+    for _, row in connections.iterrows():
+        coor_a = gates.loc[row['chip_a']]
+        coor_b = gates.loc[row['chip_b']]
+
+        horizontal_steps = coor_b['x'] - coor_a['x']
+        vertical_steps = coor_b['y'] - coor_a['y']
+        
+        plt.plot((coor_a['x'], coor_a['x'] + horizontal_steps, coor_a['x'] + horizontal_steps), (coor_a['y'], coor_a['y'], coor_a['y'] + vertical_steps), linewidth = 4, color='b')
+    
+
+    
+
+gates_path = 'gates&netlists/chip_2/print_2.csv'
+connections_path = 'gates&netlists/chip_2/netlist_7.csv'
+plot_grid(connections_path, gates_path)
 
 
-# connecties plotted (in blauw) met behulp van coordinaten van gates die verbonden
-# moeten worden (x-x y-y, zoiets)
-
-# To Do:
-# in functie zetten: voor elke input: voor elke chip moet het werken
-# connecties eerste poging
+# To Do: kruisingen tussen draden vinden, hoe?
