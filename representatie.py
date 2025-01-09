2ba8d39440cfc600508b12ea685ad59689a55608
MerelBesseling <besseling.merel@gmail.com>
2025-01-09 12:05:22 +0100

    Update representatie.py

--- representatie.py
+++ representatie.py
@@ -5,36 +5,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def grid(chip_path):
+def plot_grid(gates_path):
+    gates = pd.read_csv(gates_path)
 
-    # gates coordinaten importeren
-    gates = pd.read_csv(chip_path)
+    x_max = max(gates['x']) + 1
+    y_max = max(gates['y']) + 1
 
-    print(gates)
-    
-    max_x = max(gates['x']) + 1
-    max_y = max(gates['y']) + 1
+    for line in range(1, x_max):
+        plt.axvline(line, color = 'black', linewidth = 0.75)
 
-    # gates plotten in correcte formaat grid
-    for line in range(max_x):
-        plt.axvline(line, color = 'black')
+    for line in range(1, y_max):
+        plt.axhline(line, color = 'black', linewidth = 0.75)
 
-    for line in range(max_y):
-        plt.axhline(line, color = 'black')
+    plt.plot(gates['x'], gates['y'], 'rs', markersize = 29 - max([x_max, y_max]))
 
-    plt.plot(gates['x'], gates['y'], 'rs', markersize = 18)
-
-    for index, row in gates.iterrows():
-        plt.text(row[1]-0.2, row[2]-0.1, row[0], weight='bold', fontsize = 12)
+    for _, row in gates.iterrows():
+        plt.text(row['x'], row['y'], row['chip'], fontsize = 27 - max([x_max, y_max]), \
+            horizontalalignment='center', verticalalignment='center_baseline')
 
     plt.xticks([])
     plt.yticks([])
-    plt.ylim(0, max_y)
-    plt.xlim(0, max_x)
+    plt.xlim(0, x_max)
+    plt.ylim(0, y_max)
 
     plt.show()
 
-grid('gates&netlists/chip_2/print_2.csv')
+
+plot_grid('gates&netlists/chip_1/print_1.csv')
+
 
 # connecties plotted (in blauw) met behulp van coordinaten van gates die verbonden
 # moeten worden (x-x y-y, zoiets)
