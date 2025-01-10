aae58dc70fc198d400cdc2a823cf3c7d8a78cbc7
Amy <amy.rebk@gmail.com>
2025-01-10 13:55:35 +0100

    classes folder

--- main.py
+++ main.py
@@ -109,31 +109,6 @@ class Visualize_chip:
         plt.show()
 
 
-class Error:
-    """ This class calculates the cost of the solution. """
-    
-    def __init__(self, chip):
-        self.occupied_segments = chip.occupied_segments
-        self.gates = chip.gates.values.tolist()
-
-    def calculate_intersections(self):
-        """
-        Check if segment ends occur mulitple times in the occupied segments list. In other words,
-        check if certain point in the grid are used twice or more.
-        """
-        end_points = []
-        for segment in self.occupied_segments:
-            if list(segment[1]) not in self.gates:
-                end_points.append(segment[1])
-
-        unique_end_points = set(end_points)
-
-        return len(end_points) - len(unique_end_points)
-
-    def error_calculation(self):
-        return len(self.occupied_segments) + 300 * self.calculate_intersections()
-
-
 if __name__ == '__main__':
     gates_path = 'gates&netlists/chip_0/print_0.csv'
     connections_path = 'gates&netlists/chip_0/netlist_1.csv'
