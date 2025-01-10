f5e213b17282f50e894dff62956958a409925a39
kyra13445081 <kyra.paul@gmail.com>
2025-01-10 14:16:06 +0100

    changes in class gates

--- classes/class_Gates.py
+++ classes/class_Gates.py
@@ -4,10 +4,6 @@ class Gates:
     def __init__(self, gates_path):
         self.gates = pd.read_csv(gates_path, index_col='chip')
 
-    def get_gate_location(self, chip):
+    def gate_location(self, chip):
         """Get the location of a specific gate."""
         return self.gates.loc[chip]
-
-    def get_max_coordinates(self):
-        """Get the maximum x and y values for grid dimensions."""
-        return max(self.gates['x']) + 1, max(self.gates['y']) + 1
