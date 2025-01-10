983918bc9e4dbe80a6e5dc5949360765decc1a71
MerelBesseling <besseling.merel@gmail.com>
2025-01-10 13:46:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,20 @@ import matplotlib.pyplot as plt
 import random
 
 class Connection:
-    ''' Class that creates connections by implementing their starting location (chip a) and
-        their einding location (chip b), and lets the connections take form by taking steps in 
-        the right directions.'''
-    
-    def __init__(self, connection, gates, occupied_segments):
-        '''Implements the starting/current and end location of the connection/wire. '''
+    """
+    This class creates connections by implementing their starting location (chip a) and
+    their einding location (chip b), and lets the connections take form by taking steps in
+    the right directions.
+    """
 
+    def __init__(self, connection, gates, occupied_segments):
         self.location = gates.loc[connection['chip_a']].copy()
         self.end_location = gates.loc[connection['chip_b']]
 
         self.occupied_segments = occupied_segments
 
     def step(self, axis):
-        '''Let's the connection take a step according to the end location.'''
-
+        """ Let's the connection take a step according to the end location. """
         if self.location[axis] > self.end_location[axis]:
             new_location = self.location[axis] - 1
         elif self.location[axis] < self.end_location[axis]:
@@ -34,9 +33,10 @@ class Connection:
 
 
     def plot_and_update_values(self):
-        ''' Plots step of the connection and updates its current location (end segment becomes
-        start of segment, in next step the new end segment is determined).'''
-
+        """
+        Plot each step of the connection and updates its current location (end segment becomes
+        start of segment, in the next step the new end segment is determined).
+        """
         plt.plot((self.segment_start[0], self.segment_end[0]), (self.segment_start[1], self.segment_end[1]), linewidth = 4, color='b')
 
         self.occupied_segments.append((self.segment_start, self.segment_end))
@@ -44,10 +44,10 @@ class Connection:
 
 
     def make_connection(self):
-        '''Form the connections by taking steps. If a certain segment is already in use by
-        a connection/wire, the next step will be in the direction of the other axis. Each step
-        is plotted. '''
-
+        """
+        Form the connections by taking steps. If a certain segment is already in use by a
+        connection/wire, the next step will be in the direction of the other axis.
+        """
         while self.location['x'] != self.end_location['x'] or self.location['y'] != self.end_location['y']:
 
             while self.location['x'] != self.end_location['x']:
@@ -67,13 +67,10 @@ class Connection:
                 self.plot_and_update_values()
 
 
-class Vizualiation_chip:
-    """ This class contains the code for making the grid"""
+class Chip:
+    """ This class contains the code for making the grid. """
 
     def __init__(self, gates_path, connections_path):
-        """
-        Initialize with file paths for gates and connections
-        """
         self.gates = pd.read_csv(gates_path, index_col='chip')
         self.connections = pd.read_csv(connections_path)
 
@@ -82,10 +79,8 @@ class Vizualiation_chip:
 
         self.occupied_segments = []
 
-    def draw_grid(self):
-        """
-        Draw the grid with vertical and horizontal lines
-        """
+    def plot_grid(self):
+        """ Plot the grid with vertical and horizontal lines. """
         for line in range(1, self.x_max):
             plt.axvline(line, color='black', linewidth=0.75)
 
@@ -93,13 +88,12 @@ class Vizualiation_chip:
             plt.axhline(line, color='black', linewidth=0.75)
 
     def plot_connections(self):
+        """ Plot the connections in the connections list. """
         for _, connection in self.connections.iterrows():
             Connection(connection, self.gates, self.occupied_segments).make_connection()
 
     def plot_gates(self):
-        """
-        Plot the gates on the grid
-        """
+        """ Plot the gates on the grid. """
         plt.plot(self.gates['x'], self.gates['y'], 'rs', markersize=29 - max([self.x_max, self.y_max]))
 
         for index, row in self.gates.iterrows():
@@ -107,9 +101,7 @@ class Vizualiation_chip:
                      horizontalalignment='center', verticalalignment='center_baseline')
 
     def show_plot(self):
-        """
-        Show the grid (without the conncections yet)
-        """
+        """ Adjust the layout and show the grid. """
         plt.xticks([])
         plt.yticks([])
         plt.xlim(0, self.x_max)
@@ -118,42 +110,40 @@ class Vizualiation_chip:
 
 
 class Error:
-    '''Calculated the cost of the solution '''
+    """ This class calculates the cost of the solution. """
+    
     def __init__(self, chip):
         self.occupied_segments = chip.occupied_segments
         self.gates = chip.gates.values.tolist()
 
-    
     def calculate_intersections(self):
-        '''Check if segment ends occur mulitple times in the occupied segments list. In other words,
-        check if certain point in the grid are used twice or more.'''
-        
+        """
+        Check if segment ends occur mulitple times in the occupied segments list. In other words,
+        check if certain point in the grid are used twice or more.
+        """
         end_points = []
-
         for segment in self.occupied_segments:
             if list(segment[1]) not in self.gates:
                 end_points.append(segment[1])
 
         unique_end_points = set(end_points)
-        intersections = len(end_points) - len(unique_end_points)
 
-        return intersections
+        return len(end_points) - len(unique_end_points)
 
     def error_calculation(self):
-        error = len(self.occupied_segments) + 300 * self.calculate_intersections()
-        return error
+        return len(self.occupied_segments) + 300 * self.calculate_intersections()
+
 
 if __name__ == '__main__':
     gates_path = 'gates&netlists/chip_0/print_0.csv'
     connections_path = 'gates&netlists/chip_0/netlist_1.csv'
 
-    my_chip = Vizualiation_chip(gates_path, connections_path)
-    my_chip.draw_grid()
+    my_chip = Chip(gates_path, connections_path)
+    my_chip.plot_grid()
     my_chip.plot_connections()
     my_chip.plot_gates()
     my_chip.show_plot()
 
     costs = Error(my_chip)
     cost = costs.error_calculation()
-    print(f'The cost for this solution: {cost}')
-
+    print(f'The costs for this solution: {cost}')
