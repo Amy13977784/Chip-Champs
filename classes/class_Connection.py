f0c7b5aab147da644ecd43e5b19d972430666b5e
MerelBesseling <besseling.merel@gmail.com>
2025-01-10 14:32:36 +0100

    Update class_Connection.py

--- classes/class_Connection.py
+++ classes/class_Connection.py
@@ -15,23 +15,6 @@ class Connection:
 
         self.occupied_segments = occupied_segments
 
-    def step(self, axis):
-        """ Let's the connection take a step according to the end location. """
-        if self.location[axis] > self.end_location[axis]:
-            new_location = self.location[axis] - 1
-        elif self.location[axis] < self.end_location[axis]:
-            new_location = self.location[axis] + 1
-        else:
-            new_location = self.location[axis] + random.choice([-1, 1])
-
-        self.segment_start = (self.location['x'], self.location['y'])
-
-        if axis == 'x':
-            self.segment_end = (new_location, self.location['y'])
-        else:
-            self.segment_end = (self.location['x'], new_location)
-
-
     def plot_and_update_values(self):
         """
         Plot each step of the connection and updates its current location (end segment becomes
@@ -42,7 +25,6 @@ class Connection:
         self.occupied_segments.append((self.segment_start, self.segment_end))
         self.location.update({'x': self.segment_end[0], 'y': self.segment_end[1]})
 
-
     def make_connection(self):
         """
         Form the connections by taking steps. If a certain segment is already in use by a
@@ -51,17 +33,17 @@ class Connection:
         while self.location['x'] != self.end_location['x'] or self.location['y'] != self.end_location['y']:
 
             while self.location['x'] != self.end_location['x']:
-                self.step('x')
+                Step.make_step(self.location, self.end_location, 'x')
 
                 while (self.segment_start, self.segment_end) in self.occupied_segments or (self.segment_end, self.segment_start) in self.occupied_segments:
-                    self.step('y')
+                    Step.make_step(self.location, self.end_location, 'y')
 
                 self.plot_and_update_values()
 
             while self.location['y'] != self.end_location['y']:
-                self.step('y')
+                Step.make_step(self.location, self.end_location, 'y')
 
                 while (self.segment_start, self.segment_end) in self.occupied_segments or (self.segment_end, self.segment_start) in self.occupied_segments:
-                    self.step('x')
+                    Step.make_step(self.location, self.end_location, 'x')
 
                 self.plot_and_update_values()
