d3f1e9a18d79c45f3abaed2f99b6767340568e60
Amy <amy.rebk@gmail.com>
2025-01-13 17:28:54 +0100

    docstrings en hashtags verbeterd

--- classes/class_Connection2.py
+++ classes/class_Connection2.py
@@ -4,8 +4,8 @@ import random
 from classes import class_Step
 
 class Connection:
-    """ Class creates connections by implementing their starting location (chip a) and
-    their einding location (chip b), and lets the connections take form by taking steps in
+    """Class creates connections by implementing their starting location (chip a) and
+    their einding location (chip b), and lets the connections take form by taking random steps in
     the right directions.
     Method __init__ implements self.location, self.end_location and self.occupied_segments.
     Method plot_and_update_values plots and tracks the connections steps.
@@ -29,22 +29,28 @@ class Connection:
         self.location.update({'x': self.segment_end[0], 'y': self.segment_end[1]})
 
     def make_connection(self):
-        """Form the connections by taking steps. If a certain segment is already in use by a
-        connection/wire, the next step will be in the direction of the other axis."""
+        """Form the connections by keep initializing a step until it has reached the end gate by 
+        taking steps in a random direction (either vertical or horizontal). If gridsegment already
+        in use, step will not be 'saved' and thusa different direction is chosen in the next loop."""
 
         # while current location is not the end location (for both x and y coordinate)
         while self.location['x'] != self.end_location['x'] or self.location['y'] != self.end_location['y']:
 
+            # if both x and y values are not correct --> choose random direction
             if self.location['x'] != self.end_location['x'] and self.location['y'] != self.end_location['y']:
                 axis = random.choice(['x', 'y'])
+
+            # if x value correct --> only move in vertical direction
             elif self.location['x'] == self.end_location['x']:
                 axis = 'y'
+
+            # if y value correct --> only move in horizontal direction
             else:
                 axis = 'x'
 
-            # let line/connection take horizontal step
+            # let line/connection take a step in certain axis direction
             self.segment_start, self.segment_end = class_Step.Step(self.location, self.end_location, axis).make_step()
 
-            # If segment already occupied: retake vertical step until free gridsegment is found
+            # If segment still free
             if (self.segment_start, self.segment_end) not in self.occupied_segments and (self.segment_end, self.segment_start) not in self.occupied_segments:
                 self.plot_and_update_values()
