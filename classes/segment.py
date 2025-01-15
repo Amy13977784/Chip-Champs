e4477d354d3ffaf29cfee77810517ae256bd31b8
Amy <amy.rebk@gmail.com>
2025-01-15 11:58:44 +0100

    segment class added

---
+++ classes/segment.py
@@ -0,0 +1,7 @@
+class Segment():
+    def __init__(self, segment, occupied_segments):
+        self.segment = segment
+        self.occupied_segments = occupied_segments
+    
+    def check_segment():
+        pass
