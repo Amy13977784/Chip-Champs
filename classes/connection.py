5926e2a19cb9ed4dd9a33b284015a6cf4b9cd44e
Amy <amy.rebk@gmail.com>
2025-01-15 12:44:29 +0100

    connection class klaar

--- classes/connection.py
+++ classes/connection.py
@@ -1,19 +1,27 @@
-import random
-
 class Connection:
     """Class creates connections by implementing their starting location (chip a) and
     their einding location (chip b), and lets the connections take form by taking random steps in
     the right directions.
     Method __init__ implements self.location, self.end_location and self.occupied_segments."""
 
-    def __init__(self, connection, gates, occupied_segments):
+    def __init__(self, connection, gates):
         """Implements the starting location of the connections (the first gates, chip_a) in self.location,
         and the end destination (chip_b). It also implements all the already occuppied segment in the
         self.occupied_segments list."""
 
-        self.segments_list = []
-        self.location = gates.loc[connection['chip_a']].copy()
-        self.end_location = gates.loc[connection['chip_b']]
-        self.gates = gates
-        self.occupied_segments = occupied_segments
+        # tuple coordinates
+        self.start_location = gates[connection['chip_a']].gate
+        self.end_location = gates[connection['chip_b']].gate
+
+        # add start coordinate to list
+        self.coor_list = [self.start_location]
+
+    def add_coor(self, coor):
+        self.coor_list.append(coor)
+
+    def check_end(self):
+        return self.coor_list[-1] == self.end_location
+
+
+
    
--- classes/intersection.py
+++ classes/intersection.py
@@ -1,6 +1,3 @@
 class Intersection:
     def __init__(self, coordinates):
         self.coordinates = coordinates
-
-    def check_step(self):
-        pass
