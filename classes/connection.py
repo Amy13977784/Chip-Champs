7b77eec0011ee4b2754c1a91e3cfaa3b047bf1b7
Amy <amy.rebk@gmail.com>
2025-01-16 13:17:45 +0100

    connection class doctrings en hashtags

--- classes/connection.py
+++ classes/connection.py
@@ -1,8 +1,10 @@
 class Connection:
-    """Class creates connections by implementing their starting location (chip a) and
-    their einding location (chip b), and lets the connections take form by taking random steps in
-    the right directions.
-    Method __init__ implements self.location, self.end_location and self.occupied_segments."""
+    """Class creates a connection by implementing their starting location (chip a) and
+    their einding location (chip b), and a list of coordinates that the wire occupies.
+    Method __init__ implements self.location, self.end_location and self.coor_llsit.
+    Method add_coor adds a coordinate to self.coor_list --> adds a step decided by the 
+    algorithm to the connection. 
+    Method check_end checks if the connection has reached its end_location."""
 
     def __init__(self, start_location, end_location):
         """Implements the starting location of the connections (the first gates, chip_a) in self.location,
@@ -17,9 +19,13 @@ class Connection:
         self.coor_list = [self.start_location]
 
     def add_coor(self, coor):
+        """Adds a coordinate (a taken step) to the coordinate list (route of the wire)."""
         self.coor_list.append(coor)
 
     def check_end(self):
+        """Checks if the last coordinate in the list corresponds to the end location, if yes:
+        the wire has reached its goal/end location --> returns True, if not: wire still has
+        to take steps --> returns False."""
         return self.coor_list[-1] == self.end_location
 
 
