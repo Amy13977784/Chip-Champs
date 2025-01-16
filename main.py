43ff323d975ad50b2bb095839a347feead65a1e7
Merel Besseling <besseling.merel@gmail.com>
2025-01-16 11:58:49 +0100

    commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Visualisation of Chip & Circuit problem.
 # running the code will give the current solution and the amount of error for that particular
-# solution.
+# solution. It also displays what coordinates belong to which connection/net.
 #
 # Authors: Merel, Amy, Kyra
 
@@ -8,17 +8,17 @@ from classes import chip
 import random_algorithm
         
 if __name__ == '__main__':
-    chip_number = 0
-    netlist = 1
+    gates_path = 'data/chip_0/print_0.csv'
+    connections_path = 'data/chip_0/netlist_1.csv'
 
     for i in range(5):        
-        my_chip = chip.Chip(chip_number, netlist)
+        my_chip = chip.Chip(gates_path, connections_path)
 
         random_algorithm.Random_algorithm(my_chip).all_connections()
 
         my_chip.plot_chip()
         cost = my_chip.calculate_cost()
         print(f'The costs for this solution: {cost}')
-        my_chip.output(i, chip_number, netlist)
+        my_chip.output(i)
 
 
