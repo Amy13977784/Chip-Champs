c0493e1c81bbd02eab5ffd143743b9b0514daf59
Amy <amy.rebk@gmail.com>
2025-01-21 22:57:49 +0100

    main astar algoritme.all_connections()

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from algorithms import random_algorithm, breadth_first, astar_algorithm
         
 if __name__ == '__main__':
     chip_number = 0
-    netlist = 2
+    netlist = 3
 
-    for i in range(5):
+    for i in range(1):
         my_chip = chip.Chip(chip_number, netlist)
 
         # ----- If the connections have to be ordered by gate or Manhattan distance -----
@@ -27,14 +27,14 @@ if __name__ == '__main__':
         # breadth_first.BreadthFirst(my_chip).all_connections()
 
         # ----- A* algorithm -----
-        # astar_algorithm.Astar(my_chip)
+        astar_algorithm.Astar(my_chip).all_connections()
 
         my_chip.plot_chip()
         cost = my_chip.calculate_cost()
         print(f'The costs for this solution: {cost}')
 
         # output file: (cost, algorithm, iteration, validity), only add validity for the random algorithm
-        my_chip.output_file(cost, 'breadthfirst', i)
+        my_chip.output_file(cost, 'astar', i)
 
     # ----- if we want to plot a solution from an earlier saved file -----
     # chip.Chip(chip_number, netlist).plot_solution(0, 'breadthfirst')
