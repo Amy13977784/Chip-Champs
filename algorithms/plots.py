b2df9098e4d5b18e7ac66fb7000dd7f2e48b13ba
Merel Besseling <besseling.merel@gmail.com>
2025-01-24 16:51:18 +0100

    plot

---
+++ algorithms/plots.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+class Plots:
+
+    def distribution_plot(self, costs, title, file_or_list):
+        """ Plots the distribution of a costs list. Only works if costs is a list or a file with the list in the first column. """
+        
+        if file_or_list == 'file':
+            df = pd.read_csv(costs)
+            costs = df.iloc[:, 0].tolist()
+        
+        plt.hist(costs)
+
+        plt.title(title)
+        plt.xlabel('Kosten')
+        plt.ylabel('Frequentie')
+        plt.show()
