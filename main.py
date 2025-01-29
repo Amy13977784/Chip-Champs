ed7013393c716c10e0f7fded85a18c19b4d5b78e
kyra13445081 <kyra.paul@gmail.com>
2025-01-29 21:45:15 +0100

    main aanpassen voor beste solution vinden

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 import sys
 import argparse
+from testing_temperatures_SA import find_best_output
 from classes import chip
 from algorithms import astar, general_functions, random, breadth_first, sim_annealing, heuristics
 
@@ -22,7 +23,7 @@ def set_argument_parser():
     parser = argparse.ArgumentParser(description='Arguments')
     
     # define all the arguments
-    parser.add_argument('--solution', type=str, help='Creating or loading a solution')
+    parser.add_argument('--solution', type=str, help='Creating or loading a solution, or finding the best output file')
     parser.add_argument('--chip_number', type=str, help='Chip number (0, 1 or 2)')
     parser.add_argument('--netlist', type=str, help='Netlist number (integer from 1-9)')
     parser.add_argument('--algorithm', type=str, help='Which algorithm to use to make the connections (random, breadth_first, astar, sim annealing)')
@@ -33,8 +34,8 @@ def set_argument_parser():
 
     try: 
         if args.solution not in ['loading', 'creating']:
-            args.solution = get_input('Enter if you are creating or loading a solution: ',
-                valid_values=['loading', 'creating'], input_message='Choose loading or creating.')
+            args.solution = get_input('Enter if you are creating or loading a solution, or want to load the file with the best overall solution: ',
+                valid_values=['loading', 'creating', 'best_solution'], input_message='Choose loading, creating or best_solution.')
 
         if args.chip_number not in ['0', '1', '2']:
             args.chip_number = get_input('Enter the chip number: ',
@@ -45,16 +46,17 @@ def set_argument_parser():
             args.netlist = get_input(f'Enter the netlist: ', valid_values=netlist_options[args.chip_number],
                 input_message=f"With chip {args.chip_number} you can only choose netlists {', '.join(netlist_options[args.chip_number])}.")
 
-        if args.algorithm not in ['random', 'breadth_first', 'astar', 'sim_annealing']:
+        if args.algorithm not in ['random', 'breadth_first', 'astar', 'sim_annealing'] and args.solution not in ['best_solution']:
             args.algorithm = get_input('Enter which algorithm to make the connections with: ',
                 valid_values=['random', 'breadth_first', 'astar', 'sim_annealing'],
                 input_message='Choose from: random, breadth_first, astar or sim_annealing.')
 
-        if args.plot_chip not in ['true', 'false']:
+        if args.plot_chip not in ['true', 'false'] and args.solution not in ['best_solution']:
             args.plot_chip = get_input('Enter if you want to plot the solution: ',
                 valid_values=['true', 'false'], input_message='Choose true or false. Use lowercase letters.')
         
-        args.plot_chip = args.plot_chip.lower() == 'true'
+        if args.solution not in ['best_solution']:
+            args.plot_chip = args.plot_chip.lower() == 'true'
 
     except KeyboardInterrupt:
         print("\nInput interrupted. Exiting...")
@@ -85,6 +87,19 @@ if __name__ == '__main__':
 
     # creates the chip
     my_chip = chip.Chip(args.chip_number, args.netlist)
+
+    # returns the file with the lowest cost for a certain chip and netlist
+    if args.solution == 'best_solution':
+        find_best_output = find_best_output(output_dir="output")
+        best_cost, best_file = find_best_output.find_best_output(args.chip_number, args.netlist)
+
+        if best_file:
+            print(f"Het bestand met de beste kosten voor chip {args.chip_number} netlijst {args.netlist} is: {best_file} met kosten {best_cost}")
+
+        else:
+            print("Geen bestanden gevonden in de output map.")
+
+        sys.exit()
 
     # plots a solution from an earlier saved file
     if args.solution == 'loading':
@@ -136,7 +151,6 @@ if __name__ == '__main__':
         
         my_chip = sa.best_solution
         validity = general_functions.Functions.validity(my_chip.connections)
-
 
 
     # calculates the cost of the current solution
